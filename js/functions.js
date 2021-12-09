6debb59ba77cd7b4e73ceed70cfba9034f78eee5
Braian <braianfelipecsk123@hotmail.com>
2021-12-09 16:10:23 -0300

    atualização de imagens

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+  $(function () {
+        $('#whatsapp').floatingWhatsApp({
+            phone: '+556294901223',
+            popupMessage: 'Olá, Como posso te ajudar?',
+            message: "Olá, Gostaria de um orçamento",
+            showPopup: true,
+            showOnIE: false,
+            headerTitle: 'Fellipe Dev',
+            headerColor: '#128C7E',
+            backgroundColor: 'white',
+            buttonImage: '<img src="images/whatsapp.svg" alt="whatsapp button"/>'
+        });
+    });
