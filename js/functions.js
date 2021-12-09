300ae72a2bbcf793988db45658fbbc5cbc96193e
Braian <braianfelipecsk123@hotmail.com>
2021-12-09 16:34:25 -0300

    inserção do botão do whatsapp

--- js/functions.js
+++ js/functions.js
@@ -2,12 +2,12 @@
         $('#whatsapp').floatingWhatsApp({
             phone: '+556294901223',
             popupMessage: 'Olá, Como posso te ajudar?',
-            message: "Olá, Gostaria de um orçamento",
+            message: "Olá, Gostaria de tirar uma dúvida",
             showPopup: true,
             showOnIE: false,
-            headerTitle: 'Fellipe Dev',
-            headerColor: '#128C7E',
+            headerTitle: 'Vendedor',
+            headerColor: '#25d366',
             backgroundColor: 'white',
-            buttonImage: '<img src="images/whatsapp.svg" alt="whatsapp button"/>'
+            buttonImage: '<img src="images/whatsapp-float.svg" alt="whatsapp button"/>'
         });
     });
